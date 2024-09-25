0f2594a5459df7cc08f9fcccf949b51dac1aa79b
Lucas Tonial Scortegagna <ltonial@gmail.com>
2024-09-24 22:16:36 -0300

    [feat] Projeto criado, importado third person controller, implementado botão de action, iniciado labirinto, implementada plataforma móvel

---
+++ Assets/Scripts/Player/PlayerGroundChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerGroundChecker : MonoBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+
+    private void Start()
+    {
+        _playerInput.actions["ActionKey"].performed += ctx => ActionKey(true);
+        _playerInput.actions["ActionKey"].canceled += ctx => ActionKey(false);
+    }
+
+    private void ActionKey(bool pressedKey)
+    {
+        Debug.Log($"ActionKey = {pressedKey}");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Platform"))
+        {
+            Debug.Log($"OnTriggerEnter = {other.name}");
+            transform.parent.SetParent(other.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Platform"))
+        {
+            Debug.Log($"OnTriggerExit = {other.name}");
+            transform.parent.SetParent(null);
+        }
+    }
+}
