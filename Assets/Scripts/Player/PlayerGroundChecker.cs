3854fa38191999f8649177e387467207317d98ed
Lucas Tonial Scortegagna <ltonial@gmail.com>
2024-10-01 21:51:51 -0300

    [feat] Adicionada armadilha de bolas de ferro, adicionado exemplo de Action Area.

--- Assets/Scripts/Player/PlayerGroundChecker.cs
+++ Assets/Scripts/Player/PlayerGroundChecker.cs
@@ -1,24 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerGroundChecker : MonoBehaviour
 {
-    [SerializeField] private PlayerInput _playerInput;
-    private IGameStatus _gameStatus;
-
-    private void Start()
-    {
-        _playerInput.actions["ActionKey"].performed += ctx => ActionKey(true);
-        _playerInput.actions["ActionKey"].canceled += ctx => ActionKey(false);
-
-        _gameStatus = ServiceLocator.GetService<IGameStatus>();
-    }
-
-    private void ActionKey(bool pressedKey)
-    {
-        Debug.Log($"ActionKey = {pressedKey}");
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log($"OnTriggerEnter = {other.name}");
@@ -26,11 +9,6 @@ public class PlayerGroundChecker : MonoBehaviour
         {
             transform.parent.SetParent(other.transform);
         } 
-        else if (other.CompareTag("Dead"))
-        {
-            Debug.Log("Dead");
-            _gameStatus.InvokePlayerDeadEvent();
-        }
     }
 
     private void OnTriggerExit(Collider other)
